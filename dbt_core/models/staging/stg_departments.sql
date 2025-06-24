633cda9901715ff9bf53f34b3902e16c2b526111
zac_chen <zac.chenn@gmail.com>
2025-06-25 00:46:53 +0800

    added marts model: Time to hire

--- dbt_core/models/staging/stg_departments.sql
+++ dbt_core/models/staging/stg_departments.sql
@@ -6,6 +6,6 @@
 select
     CAST(id as INT) as id,
     CAST(name as STRING) as name,
-    CAST(company_id as INT) as id
+    CAST(company_id as INT) as company_id
 from
     {{ source('staging', 'departments_ext') }}
